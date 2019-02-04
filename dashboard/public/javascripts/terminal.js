fd84849fda270ec022c9fc33aab9287a92b84938
Eudds <coolspot1000@gmail.com>
2019-02-04 16:12:15 -0600

    Cleanup and fit terminal to entire window

--- dashboard/public/javascripts/terminal.js
+++ dashboard/public/javascripts/terminal.js
@@ -1,7 +1,12 @@
+/*
+Author: Eric Udlis
+Purpose: Handle the integrated terminal
+*/
 const os = require('os');
 const pty = require('node-pty');
 const Terminal = require('xterm').Terminal;
-
+const fit = require('xterm/lib/addons/fit/fit');
+Terminal.applyAddon(fit);
 
 const shell = process.env[os.platform() === 'win32' ? 'COMSPEC' : 'SHELL'];
 var ptyProcess = pty.spawn(shell, [], {
@@ -13,7 +18,8 @@ var ptyProcess = pty.spawn(shell, [], {
 });
 
 const xterm = new Terminal();
-xterm.open(document.getElementById('terminal'));
+xterm.open(document.getElementById('grid'));
+xterm.fit();
 
 xterm.on('data', (e) => {
     ptyProcess.write(e);
