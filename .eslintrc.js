13d2ce3b4c3ba15abc6150c9f56fa1fa7dcd8c51
Luke Houge <lukehouge@gmail.com>
2019-02-08 16:20:01 -0600

    adding unit testing and coverage with jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "no-console": "off",
     "no-alert": "off",
     "no-unused-vars": "off",
+    "no-undef": "off",
     "import/no-unresolved": "off",
     "prefer-const": "off",
     "prefer-destructuring": ["error", { "object": true, "array": false }]
