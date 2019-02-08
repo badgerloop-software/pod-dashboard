1bf5d45110f58342f29750a343d60b917b5b8d12
Eric <coolspot1000@gmail.com>
2019-02-07 20:47:28 -0600

    Commit test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*
 Author: Eric Udlis
-Purpose: Run the main Electron Process, testing submodule support
+Purpose: Run the main Electron Process
 */
 const electron = require("electron");
 const url = require("url");
