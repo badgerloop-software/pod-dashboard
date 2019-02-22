488a08fcd63f9f6c36801a95c3cc9abb91caae38
mhandler224 <mbhandler@wisc.edu>
2019-02-21 20:55:20 -0600

    changed button ids in index.html and edited/added button handlers as needed

--- src/public/javascripts/handler.js
+++ src/public/javascripts/handler.js
@@ -104,43 +104,138 @@ function fillConstants() { // eslint-disable-line no-unused-vars
 
 // State Machine Control Panel Even Listeners
 
+// Handles power off button click
+powerOff.addEventListener('click', () => {
+  console.log('powering off');
+});
+
 // Handles the archive button click
 archiveButton.addEventListener('click', () => {
   di.archiveData();
   console.log('archiving data');
 });
 
-// Handles power off button click
-one.addEventListener('click', () => {
-  console.log('powering off');
+// Handles postRun1 (magenta) button click
+postRun1.addEventListener('click', () => {
+  console.log('postRun1');
 });
 
-// Handles Idle/Ready button click
-two.addEventListener('click', () => {
+// Handles hyperloop1 (left most) button click
+hyperloop1.addEventListener('click', () => {
+  console.log('hyperloop1');
+});
+
+// Handles hyperloop2 (center) button click
+hyperloop2.addEventListener('click', () => {
+  console.log('hyperloop2');
+});
+
+// Handles hyperloop3 (right most) button click
+hyperloop3.addEventListener('click', () => {
+  console.log('hyperloop3');
+});
+
+// Handles preRun button click
+preRun.addEventListener('click', () => {
+  console.log('preRun');
+});
+
+// Handles primBrakeOn button click
+primBrakeOn.addEventListener('click', () => {
+  console.log('primary brake on');
+});
+
+// Handles primBreakOff button click
+primBrakeOff.addEventListener('click', () => {
+  console.log('primary brake off');
+});
+
+// Handles idle button click
+idle.addEventListener('click', () => {
   console.log('idling');
 });
 
-// Handles Post Run/service low speed/Safe to Approach button click
-three.addEventListener('click', () => {
-  console.log('safe to approach');
+// Handles ready button click
+ready.addEventListener('click', () => {
+  console.log('ready');
 });
 
-// Handles pumpdown/ready for pumpdown button click
-four.addEventListener('click', () => {
+// Handles serviceLowSpeed button click
+serviceLowSpeed.addEventListener('click', () => {
+  console.log('serviceLowSpeed');
+});
+
+// Handles openAir1 (left most) button click
+openAir1.addEventListener('click', () => {
+  console.log('openAir1');
+});
+
+// Handles openAir2 (center) button click
+openAir2.addEventListener('click', () => {
+  console.log('openAir2');
+});
+
+// Handles openAir3 (right most) button click
+openAir3.addEventListener('click', () => {
+  console.log('openAir3');
+});
+
+// Handles duringRun button click
+duringRun.addEventListener('click', () => {
+  console.log('duringRun');
+});
+
+// Handles hvEnable button click
+hvEnable.addEventListener('click', () => {
+  console.log('hvEnable');
+});
+
+// Handles hvDisable button click
+hvDisable.addEventListener('click', () => {
+  console.log('hvDisable');
+});
+
+// Handles readyForPumpdown button click
+readyForPumpdown.addEventListener('click', () => {
+  console.log('ready for pumpdown');
+});
+
+// Handles pumpdown button click
+pumpdown.addEventListener('click', () => {
   console.log('pumpdown');
 });
 
-// Handles hyperloop/openair/external subtrack button click
-five.addEventListener('click', () => {
-  console.log('hyperloop');
+// Handles safeToApproach button click
+safeToApproach.addEventListener('click', () => {
+  console.log('safe to approach');
 });
 
-// Handles pre/during/post run buttons
-six.addEventListener('click', () => {
-  console.log('run status');
+// Handles externalSubtrack1 (left most) button click
+externalSubtrack1.addEventListener('click', () => {
+  console.log('externalSubtrack1');
 });
 
-// Handles primary brake on/off button click
-seven.addEventListener('click', () => {
-  console.log('primary brake status');
+// Handles externalSubtrack2 (center) button click
+externalSubtrack2.addEventListener('click', () => {
+  console.log('externalSubtrack2');
+});
+
+// Handles externalSubtrack3 (right most) button click
+externalSubtrack3.addEventListener('click', () => {
+  console.log('externalSubtrack3');
+});
+
+// Handles postRun2 (Red) button click
+postRun2.addEventListener('click', () => {
+  console.log('postRun2');
+});
+
+// Handles secBrakeVentOn button click
+secBrakeVentOn.addEventListener('click', () => {
+  console.log('secondaryBrakeVentOn');
+});
+
+// Handles secBrakeVentOff button click
+secBrakeVentOff.addEventListener('click', () => {
+  console.log('secBrakeVentOff');
 });
