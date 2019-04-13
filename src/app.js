9da69f0c6e98a9ebe10fd2ae6d6fce35408ac0d3
Eudds <coolspot1000@gmail.com>
2019-04-13 15:14:39 -0500

    Removed Frame

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ function createWindow() {
   win = new BrowserWindow({
     width: 1920,
     height: 1080,
+    frame: false,
     webPreferences: { webSecurity: true, nodeIntegration: true },
   });
 
