b3205f14e95de5de1a48b28491c33b6a9ddd19f5
Andrew Janssen <janssenandrew1@gmail.com>
2019-10-09 18:09:48 -0500

    Moved scripts out of index

--- src/public/javascripts/charting.js
+++ src/public/javascripts/charting.js
@@ -2,5 +2,38 @@ let Highcharts = require('highcharts');
 
 require('highcharts/modules/exporting')(Highcharts);
 
-Highcharts.chart('container', {});
-//test chart
+function newChart(id) {
+  Highcharts.chart(id, {
+    chart: {
+    },
+    title: {
+      text: 'Fruit Consumption',
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges'],
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten',
+      },
+    },
+    exporting: {
+      enabled: false,
+    },
+    credits: {
+      enabled: false,
+    },
+    plotOptions: {
+      series: {
+        connectorAllowed: false,
+      },
+    },
+    series: [{
+      name: 'Jane',
+      data: [1, 0, 4],
+    }, {
+      name: 'John',
+      data: [5, 7, 3],
+    }],
+  });
+}
