12018dadd9930501891f6c820b9514b51b5b29f4
mhandler224 <mbhandler@wisc.edu>
2019-12-04 19:29:27 -0600

    Revert "changed script from make to build"
    This reverts commit 06510c22fb505c70472d1744bb6855f13aa54168.

--- out/podvdashboard-win32-x64/resources/app/src/cache.js
+++
@@ -1,4 +0,0 @@
-// The cache where all data gets dumped before exporting to a JSON.
-// Starts out as an empty object and gets the structure built in datainterfacing.js: createCache()
-
-module.exports = {};
