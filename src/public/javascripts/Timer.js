50beeef3e407ffa4a74f05068e3e610980e4326c
Ezra Boley <eboley@wisc.edu>
2020-08-25 12:52:56 -0500

    Changed the Timer to be OOP and fixed an issue where a timeout would create infinite instances of itself

--- src/public/javascripts/Timer.js
+++ src/public/javascripts/Timer.js
@@ -1,19 +1,34 @@
+const D = document;
+
 class Timer {
   constructor() {
     this.reference = new Date();
   }
 
   reset() {
-    console.log('reset')
     this.reference = new Date();
   }
 
   get seconds() {
-    return ((new Date() - this.reference) / 1000) % 60;
+    let seconds = Math.round(((new Date() - this.reference) % 60000) / 1000);
+
+    // Rounding sometimes means we display 60 seconds, which is non-standard.
+    //  Therefor we trick users by just hanging for an extra second.
+    //  Purely UX, doesn't change timing
+    if (seconds === 60) seconds = 59;
+    return seconds;
   }
 
   get minutes() {
     return Math.floor((new Date() - this.reference) / 60000);
+  }
+
+  display() {
+    // The math here is a little intensive so lets only do it once
+    const { minutes, seconds } = this;
+
+    if (`${seconds}`.length === 1) D.getElementById('ageDisplay').innerHTML = `${minutes}:0${seconds}`;
+    else D.getElementById('ageDisplay').innerHTML = `${minutes}:${seconds}`;
   }
 }
 
