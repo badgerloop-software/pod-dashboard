e40009115e79a632f402e05c63faf10bca7d8001
Daniel Kouchekinia <kouchekinia@wisc.edu>
2020-10-19 22:54:17 -0500

    Fixed weird windowdow typo.

--- src/app.js
+++ src/app.js
@@ -11,14 +11,14 @@ const PATH = require('path');
 let window = null;
 
 /**
- * Creates the browser windowdow and loads the index.html file in it
+ * Creates the browser window and loads the index.html file in it
  */
-function createwindowdow() {
-    // Ensure there is only ever 1 windowdow
+function createwindow() {
+    // Ensure there is only ever 1 window
     if (window !== null)
         return;
 
-    // Create the browser windowdow
+    // Create the browser window
     window = new BrowserWindow({
         width: 1920,
         height: 1080,
@@ -41,19 +41,19 @@ function createwindowdow() {
         }),
     );
 
-    // When the windowdow is closed, destroy window
+    // When the window is closed, destroy window
     window.on('closed', () => {
         window = null;
     });
 }
 
-app.on('ready', createwindowdow);
-app.on('activate', createwindowdow);
+app.on('ready', createwindow);
+app.on('activate', createwindow);
 
-// Once all windowdows have been closed, quit the app
-app.on('windowdow-all-closed', () => {
+// Once all windows have been closed, quit the app
+app.on('window-all-closed', () => {
     // Follow the MacOS convention of not quitting the application
-    // when all windowdows are closed
+    // when all windows are closed
     if (process.platform === 'darwin')
         return;
 
