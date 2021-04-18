534d8f63f77a94390b2935200b2a5ed54bf3f322
Daniel Kouchekinia <kouchekinia@wisc.edu>
2021-04-17 19:57:46 -0500

    Added HTML view backups

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ function createwindow() {
     // Load index.html by default
     window.loadURL(
         URL.format({
-            pathname: PATH.join(__dirname, 'views-pug', 'index.html'),
+            pathname: PATH.join(__dirname, 'views-orig', 'index.html'),
             protocol: 'file:',
             slashes: true,
         }),
